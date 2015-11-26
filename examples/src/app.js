f2f8e5e4422752725e2cb8fea0bff61f68b04452
neptunian <glacialgarden@gmail.com>
2015-11-26 21:34:37 +0800

    add initial code and dependencies

--- examples/src/app.js
+++ examples/src/app.js
@@ -1,15 +1,75 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Lightbox from 'react-images';
 import Gallery from 'react-photo-gallery';
+import $ from 'jquery';
+import _ from 'lodash';
 
 var App = React.createClass({
-	render: function() {
-		return (
-			<div><h1>app</h1>
-				<Gallery />
-			</div>
-		)
-	}
+    getInitialState: function(){
+        return {photos:null, pageNum:1, photosPerPage:21, containerWidth:0};
+    },
+    componentDidMount: function() {
+        this.loadMorePhotos();
+        this.loadMorePhotos = _.debounce(this.loadMorePhotos, 200);
+        window.addEventListener('scroll', this.handleScroll);
+    },
+    handleScroll: function(e){
+        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+            this.loadMorePhotos();
+        }
+    },
+    loadMorePhotos: function(e){
+        if (e){
+            e.preventDefault();
+        }
+
+        $.ajax({
+          dataType: 'jsonp',
+          jsonpCallback: 'jsonFlickrApi',
+          cache: false,
+          success: function(data) {
+            if (this.state.photos){ // if not null (not first load)
+                this.setState({
+                    photos: this.state.photos.concat(data.photos.photo),
+                    pageNum: this.state.pageNum + 1,
+                    containerWidth: React.findDOMNode(this).clientWidth
+                });
+            }
+            else{
+                this.setState({
+                    photos: data.photos.photo,
+                    pageNum: this.state.pageNum + 1,
+                    containerWidth: React.findDOMNode(this).clientWidth
+                });
+            }
+          }.bind(this),
+          error: function(xhr, status, err) {
+            console.error(status, err.toString());
+          }.bind(this)
+        });
+    },
+    render: function(){
+        if (this.state.photos){
+            return(
+		<div className="App">
+		    <div id="GalleryView" ref="galleryCont">
+			<Gallery data={this.state.photos} pageNum={this.state.pageNum} photosPerPage={this.state.photosPerPage} containerWidth={this.state.containerWidth}/>
+			<div className="loading-msg" id="msg-loading-more">Loading</div>
+		    </div>
+		</div>
+            );
+        }
+        else{
+            return(
+		<div className="App">
+		    <div id="GalleryView" ref="galleryCont">
+			<div id="msg-app-loading" className="loading-msg">Loading</div>
+		    </div>
+		</div>
+            );
+        }
+    }
 });
 
 ReactDOM.render(<App />, document.getElementById('app'));
--- src/Gallery.js
+++ src/Gallery.js
@@ -1,12 +1,108 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import Lightbox from 'react-images';
+import _ from 'lodash';
 
 var Gallery = React.createClass({
-        displayName: 'Gallery',
-	render() {
-	    return(
-		<div>Photo Gallery</div>
-	    )
+    displayName: 'Gallery',
+    getInitialState: function(){
+        return {
+	    photos:null, 
+	    containerWidth: this.props.containerWidth
 	}
+    },
+    componentDidMount: function(){
+        this.handleResize = _.debounce(this.handleResize, 400);
+        window.addEventListener('resize', this.handleResize);
+    },
+    handleResize: function(e){
+        this.setState({containerWidth: ReactDOM.findDOMNode(this).clientWidth});
+    },
+    openLightbox (index, event) {
+        event.preventDefault();
+        this.setState({
+            lightboxIsOpen: true,
+            lightboxInitialImage: index,
+        });
+    },
+    closeLightbox () {
+        this.setState({
+            lightboxIsOpen: false,
+        });
+    },
+    render: function(){
+	        // manipulate height and width data before creating PhotoPreview nodes
+        var rowLimit = 1,
+            contWidth = this.state.containerWidth - (rowLimit * 4) /* 4px for margin around each image*/,
+            photoPreviewNodes = [],
+            photosBig = [];
+            // calculate the right photo index to start looping thru based on the page
+            // i dont think i need photoNum because its only looping thru NEW images?  im not sure.
+            // but might need to change i so it doesnt loop thru the entire list again
+        // loop through each rowLimit (nth) node
+        if (window.matchMedia('(min-width: 480px)').matches){
+            rowLimit = 2;
+            contWidth = this.state.containerWidth - (rowLimit * 4); /* 4px for margin around each image*/
+        }
+        if (window.matchMedia('(min-width: 1024px)').matches){
+            rowLimit = 3;
+            contWidth = this.state.containerWidth - (rowLimit * 4); /* 4px for margin around each image*/
+        }
+        contWidth = Math.ceil(contWidth - 2); // subtract a couple pixels for unknown issue where line breaks in certain breakpoints.  this gives container some "padding"
+        for (var i=0;i<this.props.data.length;i+=rowLimit){
+            var rowItems = [];
+            // loop thru each set of rowLimit num
+            // eg. if rowLimit is 3 it will  loop thru 0,1,2, then 3,4,5 to perform calculations for the particular set
+            var ar=0,
+                totalAr=0,
+                commonHeight = 0;
+            for (var j=i; j<i+rowLimit; j++){
+                if (j == this.props.data.length){
+                    break;
+                }
+                ar=parseFloat(this.props.data[j].width_o / this.props.data[j].height_o);
+                totalAr += ar;
+                this.props.data[j].ar=ar;
+            }
+            commonHeight = contWidth / totalAr;
+            // run thru the same set of items again to give the common height
+            for (var k=i; k<i+rowLimit; k++){
+                if (k == this.props.data.length){
+                    break;
+                }
+                // gallery image
+                var src = 'https://farm4.staticflickr.com/'+this.props.data[k].server+'/'+this.props.data[k].id+'_'+this.props.data[k].secret+'.jpg';
+                // lightbox urls for react-images array
+                var src_b = 'https://farm4.staticflickr.com/'+this.props.data[k].server+'/'+this.props.data[k].id+'_'+this.props.data[k].secret+'_b.jpg';
+console.log(this.props.data[k].url_m);
+                photosBig.push(src_b);
+                photoPreviewNodes.push(
+                     <div className='PhotoPreview'>
+                        <a href="#" className={k} onClick={this.openLightbox.bind(this, k)}><img src={src} height={commonHeight} width={commonHeight * this.props.data[k].ar} alt="" /></a>
+                     </div>
+                );
+            }
+        }
+	return(
+            <div id="Gallery" className="clearfix">
+                {photoPreviewNodes}
+                <Lightbox
+                    images={photosBig}
+                    initialImage={this.state.lightboxInitialImage}
+                    isOpen={this.state.lightboxIsOpen}
+                    onClose={this.closeLightbox}
+                    width={1600}
+                    height={1600}
+                    styles={styles}
+                />
+            </div>
+        );
+
+    }
+
 });
 
+const styles={
+
+}
 module.exports = Gallery;
