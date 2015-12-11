e8d290065a15e255f7bc78371d914fca4c9242eb
neptunian <glacialgarden@gmail.com>
2015-12-11 11:31:49 -0700

    update docs, add dist

--- README.md
+++ README.md
@@ -1,33 +1,57 @@
 # React Responsive Photo Gallery
-A responsive React photo gallery component that uses [React Images] (https://github.com/jossmac/react-images) as a lightbox.
+A responsive React photo gallery component that uses [React Images](https://github.com/jossmac/react-images) as a lightbox.
 
-# Use
+## Use
 
 ```jsx
 import React from 'react';
 import Gallery from 'react-photo-gallery';
 
 export default class Sample extends React.Component {
-  render() {
-    return (
-      <Gallery photos={[gallery_src: 'http://example.com/img1_gallery.jpg', width: 681, height: 1024, ar: 1.5 , src: 'http://example.com/img1_lightbox.jpg']} />
-    );
-  }
+    render() {
+	return (
+	    <Gallery
+	      photos={PHOTO_SET}
+	    />
+	);
+    }
 }
+
+const PHOTO_SET = [
+  {
+    gallery_src: 'http://example.com/example/img1_small.jpg',
+    src: 'http://example.com/example/img1_large.jpg',
+    width: 681,
+    height: 1024,
+    ar: 1.5
+  },
+  {
+    gallery_src: 'http://example.com/example/img2_small.jpg',
+    src: 'http://example.com/example/img2_large.jpg',
+    width: 600,
+    height: 600,
+    ar: 1
+  }
+];
+
 ```
+
+### Photo properties
 
 Property        |       Type            |       Default         |       Description
 :-----------------------|:--------------|:--------------|:--------------------------------
 gallery_src     |       string    |       undefined    |       Required. Image used in the gallery collection
+src     |       string    |       undefined    |       Required. Image used for the lightbox
 width | number  | undefined  | Required. Width of the gallery image
-height  | number  | undefined | Height of the gallery image
+height  | number  | undefined | Required. Height of the gallery image
 ar | number | undefined | Required. Aspect ratio of the gallery image
-photos  | array | undefined | Required. An array of objects containing valid src and srcset values of img element
 
-# srcset
+## srcset
 
-It is possible to serve up different images to the lightbox using srcset which is supported by [React Images] (https://github.com/jossmac/react-images).  The same srcset property can be passed into the photos property.  See react-images documentation for more information on the properties.  More lightbox properties to be supported soon.
+It is possible to serve up different images to the lightbox using srcset which is supported by [React Images](https://github.com/jossmac/react-images).  The same srcset property can be passed into the photos property.  See react-images documentation for more information on the properties.  More lightbox properties to be supported soon.
 
-#Demo
+## Demo
+
+[sandygonzales.com/photography](http://sandygonzales.com/photography)
 
 
--- examples/dist/standalone.js
+++ examples/dist/standalone.js
@@ -12377,12 +12377,25 @@ var _lodash2 = _interopRequireDefault(_lodash);
 
 var Gallery = _react2['default'].createClass({
     displayName: 'Gallery',
+    propTypes: {
+        photos: _react2['default'].PropTypes.arrayOf(_react2['default'].PropTypes.shape({
+            gallery_src: _react2['default'].PropTypes.string.isRequired,
+            src: _react2['default'].PropTypes.string.isRequired,
+            srcset: _react2['default'].PropTypes.array,
+            width: _react2['default'].PropTypes.number.isRequired,
+            height: _react2['default'].PropTypes.number.isRequired,
+            ar: _react2['default'].PropTypes.number.isRequired
+        })).isRequired
+    },
     getInitialState: function getInitialState() {
         return {
-            containerWidth: this.props.containerWidth
+            currentImage: 0,
+            containerWidth: 0
         };
     },
     componentDidMount: function componentDidMount() {
+        // add 15 pixels bc for unknown reason the clientWidth here is larger than what it really is
+        this.setState({ containerWidth: _reactDom2['default'].findDOMNode(this).clientWidth - 15 });
         this.handleResize = _lodash2['default'].debounce(this.handleResize, 400);
         window.addEventListener('resize', this.handleResize);
     },
@@ -12392,35 +12405,41 @@ var Gallery = _react2['default'].createClass({
     openLightbox: function openLightbox(index, event) {
         event.preventDefault();
         this.setState({
-            lightboxIsOpen: true,
-            lightboxInitialImage: index
+            currentImage: index,
+            lightboxIsOpen: true
         });
     },
     closeLightbox: function closeLightbox() {
         this.setState({
+            currentImage: 0,
             lightboxIsOpen: false
         });
     },
+    gotoPrevious: function gotoPrevious() {
+        this.setState({
+            currentImage: this.state.currentImage - 1
+        });
+    },
+    gotoNext: function gotoNext() {
+        this.setState({
+            currentImage: this.state.currentImage + 1
+        });
+    },
     render: function render() {
-        // manipulate height and width data before creating PhotoPreview nodes
         var rowLimit = 1,
             contWidth = this.state.containerWidth - rowLimit * 4,
             /* 4px for margin around each image*/
         photoPreviewNodes = [];
-        // calculate the right photo index to start looping thru based on the page
-        // i dont think i need photoNum because its only looping thru NEW images?  im not sure.
-        // but might need to change i so it doesnt loop thru the entire list again
-        // loop through each rowLimit (nth) node
-        if (window.matchMedia('(min-width: 480px)').matches) {
+        if (this.state.containerWidth >= 480) {
             rowLimit = 2;
             contWidth = this.state.containerWidth - rowLimit * 4; /* 4px for margin around each image*/
         }
-        if (window.matchMedia('(min-width: 1024px)').matches) {
+        if (this.state.containerWidth >= 1024) {
             rowLimit = 3;
             contWidth = this.state.containerWidth - rowLimit * 4; /* 4px for margin around each image*/
         }
         contWidth = Math.ceil(contWidth - 2); // subtract a couple pixels for unknown issue where line breaks in certain breakpoints.  this gives container some "padding"
-        for (var i = 0; i < this.props.data.length; i += rowLimit) {
+        for (var i = 0; i < this.props.photos.length; i += rowLimit) {
             var rowItems = [];
             // loop thru each set of rowLimit num
             // eg. if rowLimit is 3 it will  loop thru 0,1,2, then 3,4,5 to perform calculations for the particular set
@@ -12428,28 +12447,25 @@ var Gallery = _react2['default'].createClass({
                 totalAr = 0,
                 commonHeight = 0;
             for (var j = i; j < i + rowLimit; j++) {
-                if (j == this.props.data.length) {
+                if (j == this.props.photos.length) {
                     break;
                 }
-                ar = parseFloat(this.props.data[j].width / this.props.data[j].height);
-                totalAr += ar;
-                this.props.data[j].ar = ar;
+                totalAr += this.props.photos[j].ar;
             }
             commonHeight = contWidth / totalAr;
             // run thru the same set of items again to give the common height
             for (var k = i; k < i + rowLimit; k++) {
-                if (k == this.props.data.length) {
+                if (k == this.props.photos.length) {
                     break;
                 }
-                // gallery image
-                var src = this.props.data[k].gallery_src;
+                var src = this.props.photos[k].gallery_src;
                 photoPreviewNodes.push(_react2['default'].createElement(
                     'div',
-                    { key: k, className: 'PhotoPreview' },
+                    { key: k, style: style },
                     _react2['default'].createElement(
                         'a',
                         { href: '#', className: k, onClick: this.openLightbox.bind(this, k) },
-                        _react2['default'].createElement('img', { src: src, height: commonHeight, width: commonHeight * this.props.data[k].ar, alt: '' })
+                        _react2['default'].createElement('img', { src: src, style: { display: 'block', border: 0 }, height: commonHeight, width: commonHeight * this.props.photos[k].ar, alt: '' })
                     )
                 ));
             }
@@ -12459,20 +12475,27 @@ var Gallery = _react2['default'].createClass({
             { id: 'Gallery', className: 'clearfix' },
             photoPreviewNodes,
             _react2['default'].createElement(_reactImages2['default'], {
-                images: this.props.data,
-                initialImage: this.state.lightboxInitialImage,
+                currentImage: this.state.currentImage,
+                images: this.props.photos,
                 isOpen: this.state.lightboxIsOpen,
                 onClose: this.closeLightbox,
+                onClickPrev: this.gotoPrevious,
+                onClickNext: this.gotoNext,
                 width: 1600,
                 height: 1600,
-                styles: styles
+                styles: this.props.lightboxStyles
             })
         );
     }
-
 });
+// Gallery image style
+var style = {
+    display: 'block',
+    margin: 2,
+    backgroundColor: '#e3e3e3',
+    float: 'left'
+};
 
-var styles = {};
 module.exports = Gallery;
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
