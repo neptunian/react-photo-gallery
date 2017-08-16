e1a5841cfe0a3287128c3e206555b2003ac35c9d
neptunian <glacialgarden@gmail.com>
2017-08-16 16:20:48 +0200

    replace var with const/let and fix spacing

--- examples/src/app.js
+++ examples/src/app.js
@@ -8,14 +8,14 @@ import Lightbox from 'react-images';
 
 class App extends React.Component{
     constructor(){
-	super();
+		super();
         this.state = {photos:null, pageNum:1, totalPages:1, loadedAll: false, currentImage:0};
-	this.handleScroll = this.handleScroll.bind(this);
-	this.loadMorePhotos = this.loadMorePhotos.bind(this);
-	this.closeLightbox = this.closeLightbox.bind(this);
-	this.openLightbox = this.openLightbox.bind(this);
-	this.gotoNext = this.gotoNext.bind(this);
-	this.gotoPrevious = this.gotoPrevious.bind(this);
+		this.handleScroll = this.handleScroll.bind(this);
+		this.loadMorePhotos = this.loadMorePhotos.bind(this);
+		this.closeLightbox = this.closeLightbox.bind(this);
+		this.openLightbox = this.openLightbox.bind(this);
+		this.gotoNext = this.gotoNext.bind(this);
+		this.gotoPrevious = this.gotoPrevious.bind(this);
     }
     componentDidMount() {
         this.loadMorePhotos();
@@ -23,52 +23,52 @@ class App extends React.Component{
         window.addEventListener('scroll', this.handleScroll);
     }
     handleScroll(){
-	let scrollY = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
-	if ((window.innerHeight + scrollY) >= (document.body.offsetHeight - 50)) {
-	    this.loadMorePhotos();
-	}
+		let scrollY = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
+		if ((window.innerHeight + scrollY) >= (document.body.offsetHeight - 50)) {
+	    	this.loadMorePhotos();
+		}
     }
     loadMorePhotos(e){
         if (e){
             e.preventDefault();
         }
-	if (this.state.pageNum > this.state.totalPages){
-	    this.setState({loadedAll: true});
-	    return;
-	}
+		if (this.state.pageNum > this.state.totalPages){
+	    	this.setState({loadedAll: true});
+	    	return;
+		}
         $.ajax({
           url: 'https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=372ef3a005d9b9df062b8240c326254d&photoset_id=72157680705961676&user_id=57933175@N08&format=json&per_page=21&page='+this.state.pageNum+'&extras=url_m,url_c,url_l,url_h,url_o',
           dataType: 'jsonp',
           jsonpCallback: 'jsonFlickrApi',
           cache: false,
           success: function(data) {
-	    let photos = [];
-	    data.photoset.photo.forEach(function(obj,i,array){
+		  	let photos = [];
+	    	data.photoset.photo.forEach(function(obj,i,array){
                 let aspectRatio = parseFloat(obj.width_o / obj.height_o);
-		photos.push({
+				photos.push({
                     src: (aspectRatio >= 3) ? obj.url_c : obj.url_m,
                     width: parseInt(obj.width_o),
                     height: parseInt(obj.height_o),
                     caption: obj.title,
                     alt: obj.title,
                     srcset:[
-			obj.url_m+' '+obj.width_m+'w',
+						obj.url_m+' '+obj.width_m+'w',
                         obj.url_c+' '+obj.width_c+'w',
                         obj.url_l+' '+obj.width_l+'w',
                         obj.url_h+' '+obj.width_h+'w'
                     ],
-		    sizes:[
-			'(min-width: 480px) 50vw',
-			'(min-width: 1024px) 33.3vw',
-			'100vw'
-		    ]
-		});
-	    })
-	    this.setState({
-		photos: this.state.photos ? this.state.photos.concat(photos) : photos,
-		pageNum: this.state.pageNum + 1,
-		totalPages: data.photoset.pages
-	    });
+		    		sizes:[
+						'(min-width: 480px) 50vw',
+						'(min-width: 1024px) 33.3vw',
+						'100vw'
+		    		]
+				});
+	    	});
+	    	this.setState({
+				photos: this.state.photos ? this.state.photos.concat(photos) : photos,
+				pageNum: this.state.pageNum + 1,
+				totalPages: data.photoset.pages
+	    	});
           }.bind(this),
           error: function(xhr, status, err) {
             console.error(status, err.toString());
@@ -102,49 +102,48 @@ class App extends React.Component{
         });
     }
     renderGallery(){
-	return(
-	    <Measure whitelist={['width']}>
-	    {
-		({ width }) => {
-		    var cols = 1;
-		    if (width >= 480){
-			cols = 2;
-		    }
-		    if (width >= 1024){
-			cols = 3;
-		    }
-		    return <Gallery photos={this.state.photos} cols={cols} onClickPhoto={this.openLightbox} />
-		}
-	    }
-	    </Measure>
-	);
+		return(
+	    	<Measure whitelist={['width']}>
+	    	{
+				({ width }) => {
+		    		let cols = 1;
+		    		if (width >= 480){
+						cols = 2;
+		    		}
+		    		if (width >= 1024){
+						cols = 3;
+		    		}
+		    		return <Gallery photos={this.state.photos} cols={cols} onClickPhoto={this.openLightbox} />
+				}
+	    	}
+	    	</Measure>
+		);
     }
     render(){
-	// no loading sign if its all loaded
         if (this.state.photos){
             return(
-		<div className="App">
-		    {this.renderGallery()}
-		    <Lightbox 
-			theme={{container: { background: 'rgba(0, 0, 0, 0.85)' }}}
-			images={this.state.photos}
-                        backdropClosesModal={true}
-                        onClose={this.closeLightbox}
-                        onClickPrev={this.gotoPrevious}
-                        onClickNext={this.gotoNext}
-                        currentImage={this.state.currentImage}
-                        isOpen={this.state.lightboxIsOpen}
-                        width={1600}
-                    />
-		    {!this.state.loadedAll && <div className="loading-msg" id="msg-loading-more">Loading</div>}
-		</div>
+				<div className="App">
+		    		{this.renderGallery()}
+		    		<Lightbox 
+						theme={{container: { background: 'rgba(0, 0, 0, 0.85)' }}}
+						images={this.state.photos}
+						backdropClosesModal={true}
+						onClose={this.closeLightbox}
+						onClickPrev={this.gotoPrevious}
+						onClickNext={this.gotoNext}
+						currentImage={this.state.currentImage}
+						isOpen={this.state.lightboxIsOpen}
+						width={1600}
+					/>
+		    		{!this.state.loadedAll && <div className="loading-msg" id="msg-loading-more">Loading</div>}
+				</div>
             );
         }
         else{
             return(
-		<div className="App">
-			<div id="msg-app-loading" className="loading-msg">Loading</div>
-		</div>
+				<div className="App">
+					<div id="msg-app-loading" className="loading-msg">Loading</div>
+				</div>
             );
         }
     }
--- src/Gallery.js
+++ src/Gallery.js
@@ -3,51 +3,53 @@ import PropTypes from 'prop-types';
 
 class Gallery extends React.Component{
     constructor(){
-	super();
-	this.state = {
-	    containerWidth: 0
-	};
-	this.handleResize = this.handleResize.bind(this);
+		super();
+		this.state = {
+	    	containerWidth: 0
+		};
+		this.handleResize = this.handleResize.bind(this);
     }
     componentDidMount(){
-	this.setState({containerWidth: Math.floor(this._gallery.clientWidth)})
+		this.setState({containerWidth: Math.floor(this._gallery.clientWidth)})
         window.addEventListener('resize', this.handleResize);
     }
-    componentDidUpdate(){
-	if (this._gallery.clientWidth !== this.state.containerWidth){
-	    this.setState({containerWidth: Math.floor(this._gallery.clientWidth)});
-	}
+	componentDidUpdate(){
+		if (this._gallery.clientWidth !== this.state.containerWidth){
+	    	this.setState({containerWidth: Math.floor(this._gallery.clientWidth)});
+		}
     }
     componentWillUnmount(){
-	 window.removeEventListener('resize', this.handleResize, false);
+		window.removeEventListener('resize', this.handleResize, false);
     }
     handleResize(e){
         this.setState({containerWidth: Math.floor(this._gallery.clientWidth)});
     }
     render(){
-        let cols = this.props.cols,
-            photoPreviewNodes = [],
-	    contWidth = this.state.containerWidth - (cols * (this.props.margin * 2)); 
+        const cols = this.props.cols;
+        const remainder = this.props.photos.length % cols;
+		const contWidth = Math.floor(this.state.containerWidth - (cols * (this.props.margin * 2))); 
+		let photoPreviewNodes = [];
+		let lastRowWidth;
+		let lastRowIndex;
 
-        contWidth = Math.floor(contWidth); // add some padding to prevent layout prob
-        var remainder = this.props.photos.length % cols;
         if (remainder) { // there are fewer photos than cols num in last row
-          var lastRowWidth = Math.floor( ((this.state.containerWidth / cols) * remainder) - (remainder * (this.props.margin * 2)) );
-          var lastRowIndex = this.props.photos.length - remainder;
+          lastRowWidth = Math.floor( ((this.state.containerWidth / cols) * remainder) - (remainder * (this.props.margin * 2)) );
+          lastRowIndex = this.props.photos.length - remainder;
         }
+
         // loop thru each set of  cols num
         // eg. if cols is 3 it will  loop thru 0,1,2, then 3,4,5 to perform calculations for the particular set
-        for (var i=0;i<this.props.photos.length;i+=cols){
-            var totalAr=0,
-            commonHeight = 0;
+        for (let i = 0; i<this.props.photos.length; i+= cols){
+            let totalAr = 0;
+            let commonHeight = 0;
 
-	    // get the total aspect ratio of the row
+	    	// get the total aspect ratio of the row
             for (var j=i; j<i+cols; j++){
                 if (j == this.props.photos.length){
                     break;
                 }
-		this.props.photos[j].aspectRatio = this.props.photos[j].width / this.props.photos[j].height;	
-		totalAr += this.props.photos[j].aspectRatio;
+				this.props.photos[j].aspectRatio = this.props.photos[j].width / this.props.photos[j].height;	
+				totalAr += this.props.photos[j].aspectRatio;
             }
             if (i === lastRowIndex) {
               commonHeight = lastRowWidth / totalAr;
@@ -60,26 +62,26 @@ class Gallery extends React.Component{
                     break;
                 }
 
-		let src = this.props.photos[k].src, srcset, sizes;
-		if (this.props.photos[k].srcset){
-		    srcset = this.props.photos[k].srcset.join();
-		}
-		if (this.props.photos[k].sizes){
-		    sizes = this.props.photos[k].sizes.join();
-		}
+				let src = this.props.photos[k].src, srcset, sizes;
+				if (this.props.photos[k].srcset){
+		    		srcset = this.props.photos[k].srcset.join();
+				}
+				if (this.props.photos[k].sizes){
+		    		sizes = this.props.photos[k].sizes.join();
+				}
 
-		style.margin = this.props.margin;
-		photoPreviewNodes.push(
-		    <div key={k} style={style}>
-			<a href="#" className={k} onClick={(e) => this.props.onClickPhoto(k, e)}>
-			    <img src={src} srcSet={srcset} sizes={sizes} style={{display:'block', border:0}} height={commonHeight} width={commonHeight * this.props.photos[k].aspectRatio} alt={this.props.photos[k].alt} />
-			</a>
-		    </div>
-		);
+				style.margin = this.props.margin;
+				photoPreviewNodes.push(
+		    		<div key={k} style={style}>
+						<a href="#" className={k} onClick={(e) => this.props.onClickPhoto(k, e)}>
+			    			<img src={src} srcSet={srcset} sizes={sizes} style={{display:'block', border:0}} height={commonHeight} width={commonHeight * this.props.photos[k].aspectRatio} alt={this.props.photos[k].alt} />
+						</a>
+		    		</div>
+				);
             }
         }
-	return(
-	    this.renderGallery(photoPreviewNodes)
+		return(
+	    	this.renderGallery(photoPreviewNodes)
         );
     }
     renderGallery(photoPreviewNodes){
