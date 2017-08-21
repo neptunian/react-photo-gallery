4051b97d9970e83761613ad757d9e79d1053299c
neptunian <glacialgarden@gmail.com>
2017-08-21 17:11:19 +0200

    fix line spacing and use template literal

--- examples/src/app.js
+++ examples/src/app.js
@@ -7,126 +7,135 @@ import Measure from 'react-measure';
 import Lightbox from 'react-images';
 
 class App extends React.Component{
-    constructor(){
+	constructor(){
 		super();
-        this.state = {photos:null, pageNum:1, totalPages:1, loadedAll: false, currentImage:0};
+		this.state = {photos:null, pageNum:1, totalPages:1, loadedAll: false, currentImage:0};
 		this.handleScroll = this.handleScroll.bind(this);
 		this.loadMorePhotos = this.loadMorePhotos.bind(this);
 		this.closeLightbox = this.closeLightbox.bind(this);
 		this.openLightbox = this.openLightbox.bind(this);
 		this.gotoNext = this.gotoNext.bind(this);
 		this.gotoPrevious = this.gotoPrevious.bind(this);
-    }
-    componentDidMount() {
-        this.loadMorePhotos();
-        this.loadMorePhotos = _.debounce(this.loadMorePhotos, 200);
-        window.addEventListener('scroll', this.handleScroll);
-    }
-    handleScroll(){
+	}
+	componentDidMount() {
+		this.loadMorePhotos();
+		this.loadMorePhotos = _.debounce(this.loadMorePhotos, 200);
+		window.addEventListener('scroll', this.handleScroll);
+	}
+	handleScroll(){
 		let scrollY = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
 		if ((window.innerHeight + scrollY) >= (document.body.offsetHeight - 50)) {
-	    	this.loadMorePhotos();
+			this.loadMorePhotos();
 		}
-    }
-    loadMorePhotos(e){
-        if (e){
-            e.preventDefault();
-        }
+	}
+	loadMorePhotos(e){
+		if (e){
+			e.preventDefault();
+		}
 		if (this.state.pageNum > this.state.totalPages){
-	    	this.setState({loadedAll: true});
-	    	return;
+			this.setState({loadedAll: true});
+			return;
 		}
-        $.ajax({
-          url: 'https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=372ef3a005d9b9df062b8240c326254d&photoset_id=72157680705961676&user_id=57933175@N08&format=json&per_page=21&page='+this.state.pageNum+'&extras=url_m,url_c,url_l,url_h,url_o',
-          dataType: 'jsonp',
-          jsonpCallback: 'jsonFlickrApi',
-          cache: false,
-          success: (data) => {
-	    	let photos = data.photoset.photo.map((item) => {
-                let aspectRatio = parseFloat(item.width_o / item.height_o);
-				return {
-                    src: (aspectRatio >= 3) ? item.url_c : item.url_m,
-                    width: parseInt(item.width_o),
-                    height: parseInt(item.height_o),
-                    caption: item.title,
-                    alt: item.title,
-                    srcset:[
-						item.url_m+' '+item.width_m+'w',
-                        item.url_c+' '+item.width_c+'w',
-                        item.url_l+' '+item.width_l+'w',
-                        item.url_h+' '+item.width_h+'w'
-                    ],
-		    		sizes:[
-						'(min-width: 480px) 50vw',
-						'(min-width: 1024px) 33.3vw',
-						'100vw'
-		    		]
-				};
-	    	});
-	    	this.setState({
-				photos: this.state.photos ? this.state.photos.concat(photos) : photos,
-				pageNum: this.state.pageNum + 1,
-				totalPages: data.photoset.pages
-	    	});
-          }.bind(this),
-          error: (xhr, status, err) => {
-            console.error(status, err.toString());
-          }.bind(this)
-        });
-    }
-    openLightbox(index, event){
-        event.preventDefault();
-        this.setState({
-            currentImage: index,
-            lightboxIsOpen: true
-        });
-    }
-    closeLightbox(){
-        this.setState({
-            currentImage: 0,
-            lightboxIsOpen: false,
-        });
-    }
-    gotoPrevious(){
-        this.setState({
-            currentImage: this.state.currentImage - 1,
-        });
-    }
-    gotoNext(){
-        if(this.state.photos.length - 2 === this.state.currentImage){
-            this.loadMorePhotos();
-        }
-        this.setState({
-            currentImage: this.state.currentImage + 1,
-        });
-    }
-    renderGallery(){
+		$.ajax({
+			url: 'https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos',
+			data: {
+				api_key: '372ef3a005d9b9df062b8240c326254d',
+				photoset_id: '72157680705961676',
+				user_id: '57933175@N08',
+				format: 'json',
+				per_page: '21',
+				page: this.state.pageNum,
+				extras: 'url_m,url_c,url_l,url_h,url_o'
+			},
+			dataType: 'jsonp',
+			jsonpCallback: 'jsonFlickrApi',
+			cache: false,
+			success: (data) => {
+				let photos = data.photoset.photo.map((item) => {
+					let aspectRatio = parseFloat(item.width_o / item.height_o);
+					return {
+						src: (aspectRatio >= 3) ? item.url_c : item.url_m,
+						width: parseInt(item.width_o),
+						height: parseInt(item.height_o),
+						caption: item.title,
+						alt: item.title,
+						srcset:[
+							`${item.url_m} ${item.width_m}w`,
+							`${item.url_c} ${item.width_c}w`,
+							`${item.url_l} ${item.width_l}w`,
+							`${item.url_h} ${item.width_h}w`,
+						],
+						sizes:[
+							'(min-width: 480px) 50vw',
+							'(min-width: 1024px) 33.3vw',
+							'100vw'
+						]
+					};
+				});
+				this.setState({
+					photos: this.state.photos ? this.state.photos.concat(photos) : photos,
+					pageNum: this.state.pageNum + 1,
+					totalPages: data.photoset.pages
+				});
+			}.bind(this),
+			error: (xhr, status, err) => {
+				console.error(status, err.toString());
+			}.bind(this)
+		});
+	}
+	openLightbox(index, event){
+		event.preventDefault();
+		this.setState({
+			currentImage: index,
+			lightboxIsOpen: true
+		});
+	}
+	closeLightbox(){
+		this.setState({
+			currentImage: 0,
+			lightboxIsOpen: false,
+		});
+	}
+	gotoPrevious(){
+		this.setState({
+			currentImage: this.state.currentImage - 1,
+		});
+	}
+	gotoNext(){
+		if(this.state.photos.length - 2 === this.state.currentImage){
+			this.loadMorePhotos();
+		}
+		this.setState({
+			currentImage: this.state.currentImage + 1,
+		});
+	}
+	renderGallery(){
 		return(
-	    	<Measure whitelist={['width']}>
-	    	{
+			<Measure whitelist={['width']}>
+			{
 				({ width }) => {
-		    		let cols = 1;
-		    		if (width >= 480){
+					let cols = 1;
+					if (width >= 480){
 						cols = 2;
-		    		}
-		    		if (width >= 1024){
+					}
+					if (width >= 1024){
 						cols = 3;
-		    		}
-		    		if (width >= 1824){
+					}
+					if (width >= 1824){
 						cols = 4;
-		    		}
-		    		return <Gallery photos={this.state.photos} cols={cols} onClickPhoto={this.openLightbox} />
+					}
+					return <Gallery photos={this.state.photos} cols={cols} onClickPhoto={this.openLightbox} />
 				}
-	    	}
-	    	</Measure>
+			}
+			</Measure>
 		);
-    }
-    render(){
-        if (this.state.photos){
-            return(
+	}
+	render(){
+		if (this.state.photos){
+			return(
 				<div className="App">
-		    		{this.renderGallery()}
-		    		<Lightbox 
+					{this.renderGallery()}
+					<Lightbox 
 						theme={{container: { background: 'rgba(0, 0, 0, 0.85)' }}}
 						images={this.state.photos}
 						backdropClosesModal={true}
@@ -137,18 +146,18 @@ class App extends React.Component{
 						isOpen={this.state.lightboxIsOpen}
 						width={1600}
 					/>
-		    		{!this.state.loadedAll && <div className="loading-msg" id="msg-loading-more">Loading</div>}
+					{!this.state.loadedAll && <div className="loading-msg" id="msg-loading-more">Loading</div>}
 				</div>
-            );
-        }
-        else{
-            return(
+			);
+		}
+		else{
+			return(
 				<div className="App">
 					<div id="msg-app-loading" className="loading-msg">Loading</div>
 				</div>
-            );
-        }
-    }
+			);
+		}
+	}
 };
 
 ReactDOM.render(<App />, document.getElementById('app'));
