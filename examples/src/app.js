098761d3a7e599469f3de2e1a7704f91166484c1
Sandra <glacialgarden@gmail.com>
2017-09-10 14:24:06 +0200

    remove scrolling and loading more photos from example

--- examples/src/app.js
+++ examples/src/app.js
@@ -4,48 +4,30 @@ import Gallery from 'react-photo-gallery';
 import Lightbox from 'react-images';
 import jsonp from 'jsonp';
 import Measure from 'react-measure';
-import { debounce } from './utils';
 
 import CustomImage from './CustomImage';
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = { photos: null, pageNum: 1, totalPages: 1, loadedAll: false, currentImage: 0, width: -1 };
-    this.handleScroll = this.handleScroll.bind(this);
-    this.loadMorePhotos = this.loadMorePhotos.bind(this);
+    this.state = { currentImage: 0, width: -1 };
+    this.loadPhotos = this.loadPhotos.bind(this);
     this.closeLightbox = this.closeLightbox.bind(this);
     this.openLightbox = this.openLightbox.bind(this);
     this.gotoNext = this.gotoNext.bind(this);
     this.gotoPrevious = this.gotoPrevious.bind(this);
   }
   componentDidMount() {
-    this.loadMorePhotos();
-    this.loadMorePhotos = debounce(this.loadMorePhotos, 200);
-    window.addEventListener('scroll', this.handleScroll);
+    this.loadPhotos();
   }
-  handleScroll() {
-    let scrollY = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
-    if (window.innerHeight + scrollY >= document.body.offsetHeight - 50) {
-      this.loadMorePhotos();
-    }
-  }
-  loadMorePhotos(e) {
-    if (e) {
-      e.preventDefault();
-    }
-    if (this.state.pageNum > this.state.totalPages) {
-      this.setState({ loadedAll: true });
-      return;
-    }
+  loadPhotos() {
 
     const urlParams = {
       api_key: '372ef3a005d9b9df062b8240c326254d',
       photoset_id: '72157680705961676',
       user_id: '57933175@N08',
       format: 'json',
-      per_page: '21',
-      page: this.state.pageNum,
+      per_page: '12',
       extras: 'url_m,url_c,url_l,url_h,url_o',
     };
 
@@ -74,8 +56,6 @@ class App extends React.Component {
       });
       this.setState({
         photos: this.state.photos ? this.state.photos.concat(photos) : photos,
-        pageNum: this.state.pageNum + 1,
-        totalPages: data.photoset.pages,
       });
     });
   }
@@ -97,10 +77,6 @@ class App extends React.Component {
     });
   }
   gotoNext() {
-    // load more photos if scrolling into the lightbox
-    if (this.state.photos.length - 2 === this.state.currentImage) {
-      this.loadMorePhotos();
-    }
     this.setState({
       currentImage: this.state.currentImage + 1,
     });
@@ -151,12 +127,6 @@ class App extends React.Component {
             isOpen={this.state.lightboxIsOpen}
             width={1600}
           />
-
-          {!this.state.loadedAll && (
-            <div className="loading-msg" id="msg-loading-more">
-              Loading
-            </div>
-          )}
         </div>
       );
     } else {
