c60542f32d3675f52accff38e3617cf81af084a4
Sandra <glacialgarden@gmail.com>
2017-09-13 17:06:52 +0200

    update selected component style

--- examples/src/SelectedImage.js
+++ examples/src/SelectedImage.js
@@ -1,16 +1,30 @@
 import React from 'react';
 
-const imgStyle = { display: 'block', transition: 'transform .135s cubic-bezier(0.0,0.0,0.2,1),opacity linear .15s' };
-const selectedImgStyle = { transform: 'translateZ(0px) scale3d(0.9, 0.9, 1)', transition: 'transform .135s cubic-bezier(0.0,0.0,0.2,1),opacity linear .15s'  };
-const cont = {backgroundColor: '#eee', cursor: 'pointer', overflow: 'hidden', float: 'left'}
+const imgStyle = { 
+  display: 'block', 
+  transition: 'transform .135s cubic-bezier(0.0,0.0,0.2,1),opacity linear .15s' 
+};
+const selectedImgStyle = { 
+  transform: 'translateZ(0px) scale3d(0.9, 0.9, 1)',
+  transition: 'transform .135s cubic-bezier(0.0,0.0,0.2,1),opacity linear .15s'
+};
+const cont = {
+  backgroundColor: '#eee',
+  cursor: 'pointer',
+  overflow: 'hidden',
+  float: 'left'
+}
 
 const SelectedImage = ({ index, onClick, photo, margin}) => {
   //calculate x,y scale
   const sx = (100 - ((30 / photo.width) * 100)) / 100;
   const sy = (100 - ((30 / photo.height) * 100)) / 100;
   selectedImgStyle.transform = `translateZ(0px) scale3d(${sx}, ${sy}, 1)`;
-	return (<div style={{margin, width:photo.width, ...cont}}>
+	return (<div style={{margin, width:photo.width, ...cont}} className={'cont' + (!photo.selected ? ' selected' : '')}>
     <img style={photo.selected ? {...imgStyle, ...selectedImgStyle} : {...imgStyle}} {...photo} onClick={(e) => onClick(e, {index, photo})} />
+    <style>
+    {`.cont.selected:hover{outline:2px solid #06befa}`}
+    </style>
     </div>
   )
 };
