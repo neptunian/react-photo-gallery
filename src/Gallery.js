8aed5fe9f5d7c1b3595f92197cb28d53d2439004
Sandra <glacialgarden@gmail.com>
2018-09-04 10:41:40 +0100

    return render early when no container width to save on calculations

--- src/Gallery.js
+++ src/Gallery.js
@@ -33,6 +33,8 @@ class Gallery extends React.Component {
 
   render() {
     const containerWidth = this.state.containerWidth;
+    // no containerWidth until after first render with refs, skip calculations and render nothing
+    if (!containerWidth) return <div ref={c => (this._gallery = c)}></div>;
     const { ImageComponent = Photo } = this.props;
     // subtract 1 pixel because the browser may round up a pixel
     const { margin, onClick, direction } = this.props;
