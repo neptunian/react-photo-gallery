cf377507470eebadb08e1da4030eb637c6a87de9
Sandra <glacialgarden@gmail.com>
2019-06-22 13:01:19 -0400

    update eslint for hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,23 @@
 module.exports = {
-  extends: [
-    'prettier',
-    'prettier/react'
-  ],
-  plugins: [
-    'react',
-    'prettier'
-  ],
+  extends: ['prettier', 'prettier/react'],
+  plugins: ['react', 'prettier', 'react-hooks'],
   parser: 'babel-eslint',
   env: {
     es6: true,
     node: true,
   },
   rules: {
-    'prettier/prettier': ['error', {
-      printWidth: 120,
-      tabWidth: 2,
-      singleQuote: true,
-      trailingComma: 'es5',
-      bracketSpacing: true,
-    }],
-  }
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 120,
+        tabWidth: 2,
+        singleQuote: true,
+        trailingComma: 'es5',
+        bracketSpacing: true,
+      },
+    ],
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+  },
 };
