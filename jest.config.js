089437be83428ea179706394ce5093e13f1ae6b5
Craig Jackson <igetcheapthrills@gmail.com>
2021-11-07 21:51:22 +1100

    #205 support for react 17

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   verbose: true,
+  testEnvironment: "jsdom",
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.js'],
-  setupFiles: ['./__test__/shims.js', './__test__/jestsetup.js'],
-  snapshotSerializers: ['enzyme-to-json/serializer'],
+  collectCoverageFrom: ["src/**/*.js"],
+  setupFiles: ["./__test__/shims.js", "./__test__/jestsetup.js"],
+  snapshotSerializers: ["enzyme-to-json/serializer"],
 };
